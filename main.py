e6f871cff80f5da2997e8839dccabc8968018ef9
Slavo Ng <slavo.ng99@gmail.com>
2021-07-14 19:34:35 +0200

    fix compilation errors

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 
 import flask
 from flask import request, jsonify
+import datetime as dt
 
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
@@ -64,26 +65,32 @@ def api_id():
 
 @app.route('/api/v1/ids/rating', methods=['GET'])
 def api_ids_rating():
+    version = 1
+    ts = dt.datetime.now()
     if 'orig_rating' in request.args:
         orig_rating = request.args['orig_rating']
     else:
-        return 'ERROR: field orig_rating is required.'
+        return jsonify({'input': {},
+                        'audit': {'status': 'ERROR', 'version': version, 'timestamp': ts,
+                                  'error': 'Field orig_rating is required.'}})
+    rule = None
 
     if orig_rating in ['AAA']:
         rule = 1
         stan_rating = 'AAA'
-    else if orig_rating in ['AA+','AA','AA-']:
+    elif orig_rating in ['AA+', 'AA', 'AA-']:
         rule = 2
         stan_rating = 'AA'
-    else if orig_rating in ['A+','A','A-']:
+    elif orig_rating in ['A+', 'A', 'A-']:
         rule = 3
         stan_rating = 'A'
-       /* TBD: to be done */
+    # TBD: to be done
     else:
         stan_rating = '#ND'
+    status = 'OK'
 
     return jsonify({'input': {'orig_rating': orig_rating}, 
-                    'audit': {'rule': rule, 'version': 1, 'timestamp': datetime.now()},
+                    'audit': {'rule': rule, 'version': version, 'timestamp': ts, 'status': status},
                     'output': {'stan_rating': stan_rating}})
 
 app.run()
